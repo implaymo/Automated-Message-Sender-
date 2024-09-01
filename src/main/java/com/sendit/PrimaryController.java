ea515299d58be8e8af01fc5e8d3d7e92e0071411
implaymo <goncalomoreirasilva@gmail.com>
2024-09-01 22:48:40 +0100

    Manage to make new inputs to show below the previous input when user clicks plus button.

--- src/main/java/com/sendit/PrimaryController.java
+++ src/main/java/com/sendit/PrimaryController.java
@@ -3,13 +3,15 @@ package com.sendit;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class PrimaryController {
 
     @FXML
-    private HBox container; 
+    private VBox mainContainer;
 
     @FXML
     private void plusButton() throws IOException {
@@ -22,8 +24,11 @@ public class PrimaryController {
             newInput.setPrefWidth(200.0);
             newInput.setPromptText("Write your message here");
 
-            HBox subContainer = new HBox(5);
-            container.getChildren().addAll(newAbr, newInput);
+            HBox subContainer = new HBox(10);
+            subContainer.setAlignment(Pos.CENTER);
+            subContainer.getChildren().addAll(newAbr, newInput);
+
+            mainContainer.getChildren().addAll(subContainer);
 
             
         }
