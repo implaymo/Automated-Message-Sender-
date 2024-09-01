28a4fdc4a6df0f349780e622763c56fb0bcefb9d
implaymo <goncalomoreirasilva@gmail.com>
2024-09-01 18:39:02 +0100

    Add new input when click button. Need to fix location.

--- src/main/java/com/sendit/PrimaryController.java
+++ src/main/java/com/sendit/PrimaryController.java
@@ -3,11 +3,24 @@ package com.sendit;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 public class PrimaryController {
 
     @FXML
+    private HBox container; 
+    
+    @FXML
     private void plusButton() throws IOException {
-        App.setRoot("primary");
+        try {
+            TextField newInput = new TextField();
+            newInput.setPromptText("New input");
+            System.out.println("Container children " + container.getChildren());
+            container.getChildren().add(newInput);
+        }
+        catch (Exception e) {
+            e.printStackTrace();            
+            System.out.println("ERROR " + e);
+        }
     }
 }
