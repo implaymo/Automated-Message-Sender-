ad06808e60655e0941e85b10ce27657b9fc5aa38
implaymo <goncalomoreirasilva@gmail.com>
2024-09-01 22:33:11 +0100

    Create 2 inputs instead of one, when user clicks plus button and set different width for both.

--- src/main/java/com/sendit/PrimaryController.java
+++ src/main/java/com/sendit/PrimaryController.java
@@ -5,18 +5,27 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+
 public class PrimaryController {
 
     @FXML
     private HBox container; 
-    
+
     @FXML
     private void plusButton() throws IOException {
         try {
+            TextField newAbr = new TextField();
+            newAbr.setPrefWidth(50.0);;
+            newAbr.setPromptText("Prefix here");
+
             TextField newInput = new TextField();
-            newInput.setPromptText("New input");
-            System.out.println("Container children " + container.getChildren());
-            container.getChildren().add(newInput);
+            newInput.setPrefWidth(200.0);
+            newInput.setPromptText("Write your message here");
+
+            HBox subContainer = new HBox(5);
+            container.getChildren().addAll(newAbr, newInput);
+
+            
         }
         catch (Exception e) {
             e.printStackTrace();            
