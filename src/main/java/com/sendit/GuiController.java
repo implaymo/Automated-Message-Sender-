9f76a406a7589e00c3ba61ee2218e9d18a589511
implaymo <goncalomoreirasilva@gmail.com>
2024-09-03 15:56:00 +0100

    Fix program not loading because of images not being found.

---
+++ src/main/java/com/sendit/GuiController.java
@@ -0,0 +1,21 @@
+package com.sendit;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class GuiController {
+
+    @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuBack;
+
+    @FXML
+    private AnchorPane slider;
+
+
+}
+
