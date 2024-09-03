76cc5ecaa5bfb4d24d4cb53fd77e48c733df2e2b
implaymo <goncalomoreirasilva@gmail.com>
2024-09-03 17:56:13 +0100

    Fix canvas being too big and buttons being misplaced.

--- src/main/java/com/sendit/App.java
+++ src/main/java/com/sendit/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("navigationmenu"), 1024, 720);
+        scene = new Scene(loadFXML("navigationmenu"), 700, 400);
         stage.setScene(scene);
         stage.show();
     }
