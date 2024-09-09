0fc5e2fb96a9e3ee1beafa752f5aea36a080b1f8
unknown <goncalomoreirasilva@gmail.com>
2024-09-09 22:04:30 +0100

    Initialize the creation of menu slide

--- src/main/java/com/sendit/GuiController.java
+++ src/main/java/com/sendit/GuiController.java
@@ -4,9 +4,14 @@ package com.sendit;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import javafx.scene.image.ImageView;
+import javafx.animation.TranslateTransition;
 
 public class GuiController {
 
+    @FXML
+    private ImageView Exit;
     @FXML
     private Label Menu;
 
@@ -15,6 +20,26 @@ public class GuiController {
 
     @FXML
     private AnchorPane slider;
+
+    private boolean isPanelVisible = false;
+
+    @FXML
+    private void toggleSlideMenu(){
+        TranslateTransition slideTransition = new TranslateTransition(Duration.millis(300), slider);
+        if (isPanelVisible) {
+            slideTransition.setToX(-slider.getWidth());
+        } else {
+            slideTransition.setToX(0);
+        }
+        slideTransition.play();
+        isPanelVisible = !isPanelVisible;
+        System.out.println("I CLICKED THE MENU");
+    }
+
+    @FXML 
+    public void initialize() {
+        Menu.setOnMouseClicked(event -> toggleSlideMenu());
+    }
 
 
 }
