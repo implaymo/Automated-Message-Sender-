4517b142877d11a048b5c7b12be23b7dcb9339bc
unknown <goncalomoreirasilva@gmail.com>
2024-09-10 10:45:25 +0100

    Fix menu not sliding to the left when user clicks on it. Fix system not closing when user clicks 'x'

--- src/main/java/com/sendit/GuiController.java
+++ src/main/java/com/sendit/GuiController.java
@@ -6,12 +6,16 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import javafx.scene.image.ImageView;
+
+import java.util.ResourceBundle;
+
 import javafx.animation.TranslateTransition;
 
 public class GuiController {
 
     @FXML
     private ImageView Exit;
+
     @FXML
     private Label Menu;
 
@@ -25,6 +29,7 @@ public class GuiController {
 
     @FXML
     private void toggleSlideMenu(){
+        System.out.println("I CLICKED THE MENU");
         TranslateTransition slideTransition = new TranslateTransition(Duration.millis(300), slider);
         if (isPanelVisible) {
             slideTransition.setToX(-slider.getWidth());
@@ -33,12 +38,21 @@ public class GuiController {
         }
         slideTransition.play();
         isPanelVisible = !isPanelVisible;
-        System.out.println("I CLICKED THE MENU");
     }
 
     @FXML 
     public void initialize() {
-        Menu.setOnMouseClicked(event -> toggleSlideMenu());
+        MenuBack.setOnMouseClicked(event -> {
+            System.out.println("Menu was clicked");
+            toggleSlideMenu();
+        });
+
+        Exit.setOnMouseClicked(event -> {
+            System.out.println("System exited with success");
+            System.exit(0);
+        });
+
+
     }
 
 
