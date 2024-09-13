58a2e5895760591c200c83e5818fb7411af021bc
unknown <goncalomoreirasilva@gmail.com>
2024-09-13 18:15:01 +0100

    Add users class to handle database storing. Change birthdate textfield to datefield in registration fxml file.

---
+++ src/main/java/com/sendit/Users.java
@@ -0,0 +1,13 @@
+package com.sendit;
+
+import java.util.Date;
+
+public class Users {
+
+    private int id;
+    private String email;
+    private String username;
+    private String name;
+    private Date birthday;
+    private String password;
+}
