ce7f9467e2d2be9eff21e35f02b2808003a49a0c
unknown <goncalomoreirasilva@gmail.com>
2024-09-18 21:11:24 +0100

    Made validationForm() with better readability by creating createNewLabel() for the creation of a new Label.

--- src/main/java/com/sendit/RegistrationController.java
+++ src/main/java/com/sendit/RegistrationController.java
@@ -1,12 +1,14 @@
 package com.sendit;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.passay.*;
 
@@ -19,9 +21,6 @@ public class RegistrationController {
 
     @FXML
     private VBox messagesVbox;
-
-    @FXML
-    private Label error;
 
     @FXML
     private JFXButton home;
@@ -47,6 +46,9 @@ public class RegistrationController {
     @FXML
     private Button submit;
 
+    @FXML
+    private Label newError;
+
 
 
     String formEmail;
@@ -55,6 +57,7 @@ public class RegistrationController {
     LocalDate formBirthdate;
     String formPassword;
     String formConfirmPassword;
+    PauseTransition pauseTransition = new PauseTransition(Duration.seconds(10));
     PasswordValidator validator = new PasswordValidator(
             new LengthRule(8, 16),
             new CharacterRule(EnglishCharacterData.UpperCase, 1),
@@ -63,6 +66,7 @@ public class RegistrationController {
             new CharacterRule(EnglishCharacterData.Special, 1),
             new WhitespaceRule()
     );
+
 
     public void getFormData(){
         formEmail = email.getText();
@@ -73,45 +77,51 @@ public class RegistrationController {
         formConfirmPassword = confirmpassword.getText();
     }
 
+    public void createNewLabel(String message) {
+        Label newLabel = new Label();
+        newLabel.setText(message);
+        newLabel.setStyle("-fx-text-fill: red;");
+        messagesVbox.getChildren().add(newLabel);
+    }
+
 
     public boolean validationForm() {
+        RuleResult ruleResult = validator.validate(new PasswordData(formPassword));
+        boolean isEmailValid = EmailValidator.getInstance().isValid(formEmail);
         if (formEmail.isEmpty() || formUsername.isEmpty() || formName.isEmpty() || formPassword.isEmpty()
                 || formConfirmPassword.isEmpty()) {
-            error.setText("All fields must be filled.");
+            createNewLabel("All fields must be filled.");
             return false;
         }
-
-        if (formBirthdate == null) {
-            error.setText("You must provide your Birthdate.");
+        else if (formBirthdate == null) {
+            createNewLabel("You must provide your Birthdate.");
             return false;
         }
-
-        RuleResult ruleResult = validator.validate(new PasswordData(formPassword));
-        if (!ruleResult.isValid()) {
+        else if (!ruleResult.isValid()) {
             List<String> missingRequirements = validator.getMessages(ruleResult);
             for (int i = 0; i < (missingRequirements.size() - 1); i++) {
-                Label newLabel = new Label();
-                newLabel.setLayoutX(error.getLayoutX());
-                newLabel.setLayoutY(error.getLayoutY() + 10);
-                newLabel.setText(missingRequirements.get(i));
-                messagesVbox.getChildren().add(newLabel);
+                createNewLabel(missingRequirements.get(i));
             }
             return false;
         }
-
-        if (!formPassword.equals(formConfirmPassword)) {
-            error.setText("Passwords don't match. Try again");
+        else if (!formPassword.equals(formConfirmPassword)) {
+            createNewLabel("Passwords don't match. Try again");
             return false;
         }
 
-        boolean isEmailValid = EmailValidator.getInstance().isValid(formEmail);
-        System.out.println("EMAIL VALID " + formEmail);
-        if (isEmailValid) {
-            return true;
+        else if (!isEmailValid) {
+            createNewLabel("Email invalid. Provide a valid email.");
+            return false;
         }
         else {
-            error.setText("Email invalid. Provide a valid email.");
-            return false;
+            return true;
+        }
+    }
+
+    public void removeErrorMessages() {
+        if (!validationForm()) {
+            pauseTransition.play();
+            messagesVbox.getChildren().clear();
         }
     }
 
