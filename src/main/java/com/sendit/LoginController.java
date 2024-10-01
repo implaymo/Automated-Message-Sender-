4b6da6486475892e2163fb1329841d72eefd3225
unknown <goncalomoreirasilva@gmail.com>
2024-10-01 20:21:39 +0100

    Add alert when user fails to login more than 3 times.

--- src/main/java/com/sendit/LoginController.java
+++ src/main/java/com/sendit/LoginController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -19,6 +20,7 @@ import java.io.IOException;
 public class LoginController {
 
     static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    int count = 0;
 
     @FXML
     private AnchorPane form;
@@ -73,17 +75,29 @@ public class LoginController {
         }
         boolean isPasswordValid = PBKDF2Hashing.verifyPassword(getFormPassword, checkUser.getPassword(), checkUser.getSalt(), PBKDF2Hashing.iterationCount, PBKDF2Hashing.keyLenght);
         if (!isPasswordValid) {
+            count += 1;
             createNewLabel("Credentials wrong.");
+            alertTimer();
             return false;
         }
         return true;
     }
 
+    public void alertTimer() {
+        if (count > 2) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Alert Box");
+            alert.setContentText("You failed more than 3 times. You can try again in ");
+            alert.showAndWait();
+            logger.info("Alert was created.");
+        }
+    }
 
 
     @FXML
     public void initialize() {
         Platform.runLater(() -> formUsername.requestFocus());
+
 
         form.setOnKeyPressed(event-> {
             if (event.getCode().equals(KeyCode.ENTER)) {
---
+++ src/main/java/com/sendit/Timer.java
@@ -0,0 +1,5 @@
+package com.sendit;
+
+public class Timer {
+
+}
