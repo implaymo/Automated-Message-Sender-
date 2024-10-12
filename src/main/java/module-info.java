dd3e7d3eec8e3f73f3da6f5ae31c3219c71e5189
unknown <goncalomoreirasilva@gmail.com>
2024-10-12 19:26:52 +0100

    Create GoogleCalendar.class and a method to get the next 10 events from user.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,13 @@ module com.sendit {
     requires java.desktop;
     requires java.naming;
     requires org.slf4j;
+    requires google.api.services.calendar.v3.rev411;
+    requires com.google.api.client;
+    requires google.api.client;
+    requires google.oauth.client.java6;
+    requires com.google.api.client.auth;
+    requires google.oauth.client.jetty;
+    requires com.google.api.client.json.gson;
 
     opens com.sendit to javafx.fxml, org.hibernate.orm.core;
     exports com.sendit;
